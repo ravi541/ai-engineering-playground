9252150d71efd772814a198252e1875d371e2182
tejravi-ai <60589512+ravi541@users.noreply.github.com>
2026-06-06 15:51:12 +0530

    ML
    Dataset

---
+++ MachieneLearning/ML_practice.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  6 15:01:49 2026
+
+@author: Raviteja
+"""
+
+import numpy as np #array
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+dataset = pd.read_csv(r'C:\Users\Raviteja\Downloads\14th - ML\14th - ML\5. Data preprocessing\Data.csv')
+
