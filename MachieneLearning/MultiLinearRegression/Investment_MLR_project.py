7bae5e927fd63f4d94256091acfdecd9e9621446
tejravi-ai <60589512+ravi541@users.noreply.github.com>
2026-06-08 21:06:18 +0530

    Updated MLR

---
+++ MachieneLearning/MultiLinearRegression/Investment_MLR_project.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset=pd.read_csv(r"C:\Users\Raviteja\AVSCODE\ai-engineering-playground\MachieneLearning\MultiLinearRegression\Investment.csv")
+
+x=dataset.iloc[:,:-1]
+y=dataset.iloc[:,4]
+
+x=pd.get_dummies(x,dtype=int)
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+y_pred = regressor.predict(X_test)
+
+c= regressor.intercept_
+m=regressor.coef_
