f1aad2aefa730401bdca1e676f55defdd3fe384e
tejravi-ai <60589512+ravi541@users.noreply.github.com>
2026-07-05 16:40:29 +0530

    Updated LogisticRegression

--- MachieneLearning/Classification/LogisticRegression/logisticRegression.py
+++ MachieneLearning/Classification/LogisticRegression/logisticRegression.py
@@ -11,11 +11,12 @@ y= dataset.iloc[:,-1].values
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
 
+'''
 from sklearn.preprocessing import StandardScaler
 sc=StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test= sc.transform(X_test)
-
+'''
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression()
 classifier.fit(X_train, y_train)
@@ -29,3 +30,9 @@ print(cm)
 from sklearn.metrics import accuracy_score
 ac = accuracy_score(y_test, y_pred)
 print(ac)
+
+bias = classifier.score(X_train, y_train)
+print(bias)
+
+variance = classifier.score( X_test, y_test)
+print(variance)
