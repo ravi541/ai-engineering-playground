6f3fbebde29785fc8b26e0f704b822893932d7fc
tejravi-ai <60589512+ravi541@users.noreply.github.com>
2026-07-05 17:38:38 +0530

    LOGISTIC REGRESSION with roc and auc

--- MachieneLearning/Classification/LogisticRegression/logisticRegression.py
+++ MachieneLearning/Classification/LogisticRegression/logisticRegression.py
@@ -11,12 +11,12 @@ y= dataset.iloc[:,-1].values
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
 
-'''
+
 from sklearn.preprocessing import StandardScaler
 sc=StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test= sc.transform(X_test)
-'''
+
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression()
 classifier.fit(X_train, y_train)
@@ -36,3 +36,66 @@ print(bias)
 
 variance = classifier.score( X_test, y_test)
 print(variance)
+
+
+# Future prediction 
+# Session 64
+
+dataset1 = pd.read_csv(r"C:\Users\Raviteja\Downloads\15. Logistic regression with future prediction\15. Logistic regression with future prediction\Future prediction1.csv")
+
+d2 = dataset1.copy()
+
+dataset1 = dataset1.iloc[:,[2,3]].values
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+M = sc.fit_transform(dataset1)
+
+y_pred1=pd.DataFrame()
+
+d2['y_pred1']=classifier.predict(M)
+
+d2.to_csv('final1.csv')
+
+#roc and aoc
+
+from sklearn.metrics import roc_auc_score,roc_curve
+y_pred_prob = classifier.predict_proba(X_test)[:,1]
+
+auc_score = roc_auc_score(y_test,y_pred_prob)
+auc_score
+
+fpr,tpr,thresholds = roc_curve(y_test, y_pred_prob)
+
+plt.figure(figsize=(8,6))
+plt.plot(fpr,tpr,label=f'lOGISTIC REGRESSION (AUC = {auc_score:.2f})')
+plt.plot(([0,1],[0,1],'k--'))
+plt.xlabel('False positive rate')
+plt.ylabel('True Positive rate')
+plt.title('ROC curve')
+plt.legend(loc='lower right')
+plt.grid()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
